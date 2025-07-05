9ee36b263ff79550411e481f2e5462c823c7c0a8
Matvey Ivanov <matvenoid@gmail.com>
2025-07-05 12:39:43 +0700

    chore: Add docker-compose and .env.template

---
+++ init-scripts/init.sql
@@ -0,0 +1,22 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
+    birthday DATE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE OR REPLACE FUNCTION update_updated_at() 
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_updated_at
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
--- src/main/kotlin/infrastructure/config/RedisProvider.kt
+++ src/main/kotlin/infrastructure/config/RedisProvider.kt
@@ -5,8 +5,7 @@ import io.lettuce.core.api.sync.RedisCommands
 
 object RedisProvider {
     private val host = System.getenv("REDIS_HOST")
-    private val port = System.getenv("REDIS_PORT")
-    private val client = RedisClient.create("redis://$host:$port")
+    private val client = RedisClient.create("redis://$host:6379")
     private val connection = client.connect()
 
     val commands: RedisCommands<String, String> = connection.sync()
